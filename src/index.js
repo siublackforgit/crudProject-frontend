d8784f991c90788cdb49d2372a850fb5481d688f
Ricky Yeung <70504085+siublackforgit@users.noreply.github.com>
2023-02-11 08:22:42 +0800

    hashhistory-correct2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {BrowserRouter,Routes,Route,hashHistory} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter history={hasHistory}>
+    <BrowserRouter history={hashHistory}>
     <Routes>
       <Route path="/" element={<App/>} />
       <Route path="/createpost" element={<CreatePost/>}/>
